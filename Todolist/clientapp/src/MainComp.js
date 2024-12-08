ed5bfaa23a48b874b1e5ef34eb73a2e09738d90e
Alankc <122060247+sennsy12@users.noreply.github.com>
2024-12-08 13:26:15 +0100

    aq

--- Todolist/clientapp/src/MainComp.js
+++ Todolist/clientapp/src/MainComp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Sidebar from './components/Sidebar';
 
